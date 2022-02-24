cd1850e31dbedb5a3510f102810203982ddbd394
Amaan Q <amaanq12@gmail.com>
2022-02-23 20:31:57 -0500

    better examples

--- examples/main.go
+++ examples/main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/amaanq/coc.go"
 )
 
 func main() {
-	client, err := coc.New(map[string]string{"username": "password"})
-	if err != nil {
-		panic(err)
+	client, cErr := coc.New(map[string]string{"username": "password"})
+	if cErr != nil {
+		panic(cErr)
 	}
 
 	player, err := client.GetPlayer("#2PP")
+	if err.Err() != nil {
+		fmt.Println(err.Message)
+	}
+
+	fmt.Println("Player name: ", player.Name)
+	client.SearchClans()
 }
