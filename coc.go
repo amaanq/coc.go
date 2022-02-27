785db469dc497f522707b16b9283648ffff3a1c6
Amaan Q <amaanq12@gmail.com>
2022-02-26 22:37:49 -0500

    exported correct tag

--- coc.go
+++ coc.go
@@ -4,6 +4,12 @@ import (
 	"github.com/amaanq/coc.go/client"
 )
 
+// Pass in a map which maps a username to a password
 func New(credentials map[string]string) (*client.HTTPSessionManager, error) {
 	return client.New(credentials)
+}
+
+// this function is inside /client/ but this makes it easier to use outside of the client package. 
+func CorrectTag(tag string) string {
+	return client.CorrectTag(tag)
 }
