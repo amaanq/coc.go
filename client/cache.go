6989257a18d54e8a097edaa6091b7f7d783fcb3a
amaanq <amaanq12@gmail.com>
2022-04-10 15:25:39 -0400

    got rid of badger db, use local brotli caching...

--- client/cache.go
+++ client/cache.go
@@ -1,50 +1,104 @@
 package client
 
 import (
+	"bytes"
+	"errors"
+	"io/ioutil"
+	"sync"
 	"time"
 
-	badger "github.com/dgraph-io/badger/v3"
-)
+	"github.com/andybalholm/brotli"
+	// badger "github.com/dgraph-io/badger/v3"
+) // fuck badger
 
 var (
-	cache *badger.DB
+	cache = make(map[string][]byte)
+	cacheLock sync.RWMutex
 )
 
-func init() {
-	opt := badger.DefaultOptions("").WithInMemory(true)
-	opt.Logger = nil
-	_cache, err := badger.Open(opt)
-	if err != nil {
-		panic(err)
+// func init() {
+// 	opt := badger.DefaultOptions("").WithInMemory(true)
+// 	opt.Logger = nil
+// 	_cache, err := badger.Open(opt)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	cache = _cache
+// }
+
+func getFromCache(key string) ([]byte, error) {
+	cacheLock.RLock()
+	data, ok := cache[key]
+	cacheLock.RUnlock()
+	if ok {
+		data, err := readFromBrotli(data)
+		return data, err
 	}
-	cache = _cache
+	return nil, errors.New("not in cache")
 }
 
-func getFromCache(url string) ([]byte, error) {
-	var valCopy []byte
-	err := cache.View(func(txn *badger.Txn) error {
-		item, err := txn.Get([]byte(url))
-		if err != nil {
-			return err
-		}
+func writeToCache(key string, data []byte, duration int) error {
+	brot, err := writeToBrotli(data)
+	if err != nil {
+		return err 
+	}
+	cacheLock.Lock()
+	cache[key] = brot
+	cacheLock.Unlock()
+	if duration > 0 {
+		go func() {
+			time.Sleep(time.Second * time.Duration(duration))
+			cacheLock.Lock()
+			delete(cache, key)
+			cacheLock.Unlock()
+		}()
+	}
+	return nil
+}
 
-		valCopy, err = item.ValueCopy(nil)
-		if err != nil {
-			return err
-		}
-
-		return nil
-	})
+func writeToBrotli(data []byte) ([]byte, error) {
+	buf := bytes.NewBuffer(nil)
+	writer := brotli.NewWriter(buf)
+	_, err := writer.Write(data)
 	if err != nil {
 		return nil, err
 	}
-	return valCopy, nil
+	writer.Close()
+	return buf.Bytes(), nil
 }
 
-func writeToCache(url string, data []byte, duration int) error {
-	err := cache.Update(func(txn *badger.Txn) error {
-		entry := badger.NewEntry([]byte(url), data).WithTTL(time.Second * time.Duration(duration))
-		return txn.SetEntry(entry)
-	})
-	return err
+func readFromBrotli(brotData []byte) ([]byte, error) {
+	buf := bytes.NewReader(brotData)
+	reader := brotli.NewReader(buf)
+	return ioutil.ReadAll(reader)
 }
+
+
+// func getFromCache(url string) ([]byte, error) {
+// 	var valCopy []byte
+// 	err := cache.View(func(txn *badger.Txn) error {
+// 		item, err := txn.Get([]byte(url))
+// 		if err != nil {
+// 			return err
+// 		}
+
+// 		valCopy, err = item.ValueCopy(nil)
+// 		if err != nil {
+// 			return err
+// 		}
+
+// 		return nil
+// 	})
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return valCopy, nil
+// }
+
+// func writeToCache(url string, data []byte, duration int) error {
+// 	err := cache.Update(func(txn *badger.Txn) error {
+// 		entry := badger.NewEntry([]byte(url), data).WithTTL(time.Second * time.Duration(duration))
+// 		return txn.SetEntry(entry)
+// 	})
+// 	return err
+// }
--- go.mod
+++ go.mod
@@ -7,27 +7,15 @@ require (
 	github.com/joho/godotenv v1.4.0
 )
 
+require github.com/stretchr/testify v1.4.0 // indirect
+
 require (
-	github.com/cespare/xxhash/v2 v2.1.1 // indirect
-	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
-	github.com/golang/groupcache v0.0.0-20190702054246-869f871628b6 // indirect
-	github.com/golang/snappy v0.0.3 // indirect
-	github.com/google/flatbuffers v1.12.1 // indirect
 	github.com/json-iterator/go v1.1.12
-	github.com/klauspost/compress v1.12.3 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	go.opencensus.io v0.22.5 // indirect
 )
 
 require (
-	github.com/cespare/xxhash v1.1.0 // indirect
-	github.com/dgraph-io/badger/v3 v3.2103.2
-	github.com/dgraph-io/ristretto v0.1.0 // indirect
-	github.com/dustin/go-humanize v1.0.0 // indirect
-	github.com/golang/protobuf v1.3.1 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/andybalholm/brotli v1.0.4
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
-	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 )
