2a5d928d986c94fb7e57799d8693d2b75c185f80
Anton Schnitzer <32676027+antonschnitzer@users.noreply.github.com>
2023-08-23 22:08:01 +0200

    Fixed achievement.go
    1. Fixed name of achievement WellSeasoned
    2. Added 7 new achievements from TH15/Clan capital update
    3. Ordered the achievements the way they are displayed in clash of clans to make spotting missing achievements easier

--- achievement.go
+++ achievement.go
@@ -5,59 +5,68 @@ import "errors"
 type AchievementID string
 
 type Achievement struct {
-	Name           string
-	Info           string
-	CompletionInfo string
-	Village        string
-	Stars          int
-	Value          int
-	Target         int
+	Name           string `json:"name"`
+	Info           string `json:"info"`
+	CompletionInfo string `json:"completionInfo"`
+	Village        string `json:"village"`
+	Stars          int    `json:"stars"`
+	Value          int    `json:"value"`
+	Target         int    `json:"target"`
 }
 
 // If you're peering into this and I've typo'd an achievement name causing an error when fetching it, please let me know.
 var (
 	None                    Achievement = Achievement{}
-	BiggerCoffers           Achievement = Achievement{Name: "Bigger Coffers"}
-	GetThoseGoblins         Achievement = Achievement{Name: "Get those Goblins!"}
+	KeepYourAccountSafeOld  Achievement = Achievement{Name: "Keep Your Account Safe!", Info: "Protect your village by connecting to a social network"}
+	KeepYourAccountSafeSCID Achievement = Achievement{Name: "Keep Your Account Safe!", Info: "Connect your account to Supercell ID for safe keeping."}
 	BiggerAndBetter         Achievement = Achievement{Name: "Bigger & Better"}
-	NiceAndTidy             Achievement = Achievement{Name: "Nice and Tidy"}
 	DiscoverNewTroops       Achievement = Achievement{Name: "Discover New Troops"}
+	BiggerCoffers           Achievement = Achievement{Name: "Bigger Coffers"}
 	GoldGrab                Achievement = Achievement{Name: "Gold Grab"}
 	ElixirEscapade          Achievement = Achievement{Name: "Elixir Escapade"}
-	SweetVictory            Achievement = Achievement{Name: "Sweet Victory!"}
-	EmpireBuilder           Achievement = Achievement{Name: "Empire Builder"}
-	WallBuster              Achievement = Achievement{Name: "Wall Buster"}
-	Humiliator              Achievement = Achievement{Name: "Humiliator"}
-	UnionBuster             Achievement = Achievement{Name: "Union Buster"}
-	Conqueror               Achievement = Achievement{Name: "Conqueror"}
-	Unbreakable             Achievement = Achievement{Name: "Unbreakable"}
-	FriendInNeed            Achievement = Achievement{Name: "Friend in Need"}
-	MortarMauler            Achievement = Achievement{Name: "Mortar Mauler"}
 	HeroicHeist             Achievement = Achievement{Name: "Heroic Heist"}
+	WellSeasoned            Achievement = Achievement{Name: "Well Seasoned"}
+	NiceAndTidy             Achievement = Achievement{Name: "Nice and Tidy"}
+	EmpireBuilder           Achievement = Achievement{Name: "Empire Builder"}
+	ClanWarWealth           Achievement = Achievement{Name: "Clan War Wealth"}
+	FriendInNeed            Achievement = Achievement{Name: "Friend in Need"}
+	SharingIsCaring         Achievement = Achievement{Name: "Sharing is caring"}
+	SiegeSharer             Achievement = Achievement{Name: "Siege Sharer"}
+	WarHero                 Achievement = Achievement{Name: "War Hero"}
+	WarLeagueLegend         Achievement = Achievement{Name: "War League Legend"}
+	GamesChampion           Achievement = Achievement{Name: "Games Champion"}
+	Unbreakable             Achievement = Achievement{Name: "Unbreakable"}
+	SweetVictory            Achievement = Achievement{Name: "Sweet Victory!"}
+	Conqueror               Achievement = Achievement{Name: "Conqueror"}
 	LeagueAllStar           Achievement = Achievement{Name: "League All-Star"}
+	Humiliator              Achievement = Achievement{Name: "Humiliator"}
+	NotSoEasyThisTime       Achievement = Achievement{Name: "Not So Easy This Time"}
+	UnionBuster             Achievement = Achievement{Name: "Union Buster"}
+	BustThis                Achievement = Achievement{Name: "Bust This"}
+	WallBuster              Achievement = Achievement{Name: "Wall Buster"}
+	MortarMauler            Achievement = Achievement{Name: "Mortar Mauler"}
 	XBowExterminator        Achievement = Achievement{Name: "X-Bow Exterminator"}
 	Firefighter             Achievement = Achievement{Name: "Firefighter"}
-	WarHero                 Achievement = Achievement{Name: "War Hero"}
-	ClanWarWealth           Achievement = Achievement{Name: "Clan War Wealth"}
 	AntiArtillery           Achievement = Achievement{Name: "Anti-Artillery"}
-	SharingIsCaring         Achievement = Achievement{Name: "Sharing is caring"}
-	KeepYourAccountSafeOld  Achievement = Achievement{Name: "Keep Your Account Safe!", Info: "Protect your village by connecting to a social network"}
-	MasterEngineering       Achievement = Achievement{Name: "Master Engineering"}
-	NextGenerationModel     Achievement = Achievement{Name: "Next Generation Model"}
-	UnBuildIt               Achievement = Achievement{Name: "Un-Build It"}
-	ChampionBuilder         Achievement = Achievement{Name: "Champion Builder"}
-	HighGear                Achievement = Achievement{Name: "High Gear"}
-	HiddenTreasures         Achievement = Achievement{Name: "Hidden Treasures"}
-	GamesChampion           Achievement = Achievement{Name: "Games Champion"}
-	DragonSlayer            Achievement = Achievement{Name: "Dragon Slayer"}
-	WarLeagueLegend         Achievement = Achievement{Name: "War League Legend"}
-	KeepYourAccountSafeSCID Achievement = Achievement{Name: "Keep Your Account Safe!", Info: "Connect your account to Supercell ID for safe keeping."}
-	WellSeasoned            Achievement = Achievement{Name: "Season Challenges"}
 	ShatteredAndScattered   Achievement = Achievement{Name: "Shattered and Scattered"}
-	NotSoEasyThisTime       Achievement = Achievement{Name: "Not So Easy This Time"}
-	BustThis                Achievement = Achievement{Name: "Bust This"}
+	Counterspell            Achievement = Achievement{Name: "Counterspell"}
+	MonolithMasher          Achievement = Achievement{Name: "Monolith Masher"}
+	GetThoseGoblins         Achievement = Achievement{Name: "Get those Goblins!"}
+	GetThoseOtherGoblins    Achievement = Achievement{Name: "Get those other Goblins!"}
+	GetEvenMoreGoblins      Achievement = Achievement{Name: "Get even more Goblins!"}
+	DragonSlayer            Achievement = Achievement{Name: "Dragon Slayer"}
+	UngratefulChild         Achievement = Achievement{Name: "Ungrateful Child"}
 	SuperbWork              Achievement = Achievement{Name: "Superb Work"}
-	SiegeSharer             Achievement = Achievement{Name: "Siege Sharer"}
+
+	MasterEngineering   Achievement = Achievement{Name: "Master Engineering"}
+	HiddenTreasures     Achievement = Achievement{Name: "Hidden Treasures"}
+	HighGear            Achievement = Achievement{Name: "High Gear"}
+	NextGenerationModel Achievement = Achievement{Name: "Next Generation Model"}
+	UnBuildIt           Achievement = Achievement{Name: "Un-Build It"}
+	ChampionBuilder     Achievement = Achievement{Name: "Champion Builder"}
+
+	AggressiveCapitalism Achievement = Achievement{Name: "Aggressive Capitalism"}
+	MostValuableClanmate Achievement = Achievement{Name: "Most Valuable Clanmate"}
 )
 
 func GetAchievement(givenAchievements []Achievement, wantedAchievement Achievement) (Achievement, error) {
